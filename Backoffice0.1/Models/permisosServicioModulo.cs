374f0239f87452b24ea810f17ec24c5f537f20fd
Hiram-Hexaware <hiramyanek@gmail.com>
2018-05-25 09:17:54 -0500

    Agregar modelos base

---
+++ Backoffice0.1/Models/permisosServicioModulo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Backoffice0._1.Models
+{
+    public class permisosServicioModulo
+    {
+        public int id_modulo { get; set; }
+        public int id_servicio { get; set; }
+        public string id_permiso { get; set; }
+    }
+}
